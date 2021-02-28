5d9b8f166645d80562ddba8d11a47ab350ad0661
Nipuni Obe <obe@gmail.com>
2021-02-28 14:20:17 -0500

    did same as to do list

---
+++ EU/DetailTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailTableViewController.swift
+//  EU
+//
+//  Created by Nipuni Obe on 2/28/21.
+//
+
+import UIKit
+
+class DetailTableViewController: UITableViewController {
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var countryField: UITextField!
+    @IBOutlet weak var capitalField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+         
+    }
+
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+}
