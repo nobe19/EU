c7cd4e971cfa3a94c4366c7b77a8adba9d633a94
Nipuni Obe <obe@gmail.com>
2021-02-28 21:25:52 -0500

    5.6 completed

---
+++ EU/Nation.swift
@@ -0,0 +1,14 @@
+//
+//  Nation.swift
+//  EU
+//
+//  Created by Nipuni Obe on 2/28/21.
+//
+
+import Foundation
+
+struct Nation {
+    var country: String
+    var capital: String
+    var usesEuro: Bool
+}
